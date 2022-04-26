941bd9c1fce4ec0e1e0cbbea63ee4fe7f83d806c
OH haeseung <gotmd318@gmail.com>
2022-04-26 18:21:16 +0900

    4월 26일 수업내용

---
+++ pypro3/anal5/std_sample.py
@@ -0,0 +1,22 @@
+# 표준편차, 분산의 중요성 : 데이터의 분포 파악
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+
+np.random.seed(1)
+print(stats.norm(loc = 1, scale = 2).rvs(10))  # loc 기대값 scale 표준편차 
+
+print('---------------'*10)
+centers = [1,1.5,2]
+col = 'rgb'
+
+std = 0.05      # 표준편차 0.1 1 ... 
+datas = []
+
+for i in range(3):
+    datas.append(stats.norm(loc = centers[i], scale = std).rvs(100))
+    plt.plot(np.arange(100) + i *100, datas[i], '*', color = col[i])
+    
+plt.show()
